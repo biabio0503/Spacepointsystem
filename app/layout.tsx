3eec41b076d7903c6fd806846ae4acb7c7b95bfd
jaehoya <hmujaeho@gmail.com>
2026-02-24 14:26:10 +0900

    style. title 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "@/styles/index.css";
 import { AppProvider } from "@/components/context/AppContext";
 
 export const metadata: Metadata = {
-   title: "Buddy Point System",
-   description: "포인트 관리 시스템",
+   title: "Space Point System",
+   description: "서울과학기술대학교 제42대 SPACE 학생복지위원회 포인트 관리 시스템",
 };
 
 export default function RootLayout({
